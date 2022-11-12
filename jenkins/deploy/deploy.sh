a18908f8504c8cfc627e3d012d313f82d5dfc750
Ikeoluwa Joshua Kolawole <kolawoleikeoluwa2018@gmail.com>
2022-11-12 01:40:19 +0000

    fix bug in deployment script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,9 +5,8 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 # copy file to aws production server using ssh key
-scp -i /opt/prod /tmp/.auth prod-user@44.201.197.10:/tmp/.auth 
-scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@44.201.197.10:/tmp/publish
+# ensure the private key file has the right user permissions
+scp -i /opt/prod /tmp/.auth prod-user@3.91.252.110:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@3.91.252.110:/tmp/publish
 # run remote command using ssh
-ssh -i /opt/prod prod-user@44.201.197.10
-cd /tmp/
-./publish.sh
+ssh -i /opt/prod prod-user@3.91.252.110 ". /tmp/publish"
